39e2c518ff1a9f1197bb5bab25721b82fe86374b
shawnliu <liuyihsuan1012@gmail.com>
2022-08-26 11:48:32 +0800

    html及css製作中

---
+++ script.js
@@ -0,0 +1,31 @@
+// const lesson = document.getElementsByClassName('lesson');
+// const lesson = document.querySelectorAll('lesson');
+// console.log(lesson);
+// const mainVideo = document.querySelectorAll('main-Video')
+// console.log(mainVideo);
+
+
+// const test = document.querySelector('h3');
+// console.log(test);
+// const lessonTitle = document.querySelectorAll('lessonTitle');
+// console.log(lessonTitle);
+
+// 有抓取到lesson
+// test.textContent = "bbb";
+// function myFunction() {
+//     alert("第一堂");
+//     // TODO 抓取mainVideo節點 並變更內容
+//     // console.log(mainVideo.textContent);
+//     // mainVideo.textContent = "sorry";
+//     // console.log(mainVideo.textContent);
+//     console.log(mainVideo);
+//     mainVideo.innerHTML = '<h1 class=".header">被點擊了<span>1</span></h1>';
+// }
+// function myFunction() {
+//     test.innerText = "Hello World";
+// }
+// function showAlert() {
+//     alert("show function!")
+// }
+
+// lesson.addEventListener("click", showAlert);
